d11783972d9e42b7c67d09d0623f800a39bf5566
Koji Ishii <kojii@chromium.org>
2025-05-04 16:35:50 +0900

    [ci] Add `rust fmt` and `rust clippy`

---
+++ hooks/precommit.sh
@@ -0,0 +1,6 @@
+set -x
+cargo test
+# cargo fmt --all --check
+cargo fmt --all
+# cargo clippy --all-targets --all-features -- -D warnings
+cargo clippy --fix --allow-dirty --all-targets --all-features -- -D warnings
