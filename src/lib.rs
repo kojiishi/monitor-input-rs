1b5af71e2e1fd86dedca43f7b302b8507138d331
Koji Ishii <kojii@chromium.org>
2025-06-29 02:28:00 +0900

    doc: Update documentations

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+//! This crate contains a command line tool and its library
+//! to change input sources of display monitors with [DDC/CI].
+//!
+//! * The [`Cli`] struct provides the command line tool.
+//! * The [`Monitor`] struct provides functions to
+//!   change input sources of display monitors.
+//!
+//! [DDC/CI]: https://en.wikipedia.org/wiki/Display_Data_Channel
 mod cli;
 pub use cli::*;
 
